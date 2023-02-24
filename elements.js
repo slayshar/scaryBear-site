628a81ee00661d8d4af2a1af5ac68e9b6ec73e50
Porsa <porsa@mail.ru>
2023-02-24 11:04:54 +0300

    now we have all the variables in the elements.js

---
+++ elements.js
@@ -0,0 +1,45 @@
+const whiteImg = `https://cdn1.ozone.ru/s3/multimedia-o/c1000/6040136676.jpg`;
+const blackImg = `https://i.ibb.co/Kxmb0sS/6040136676.jpg`;
+const cursedImg = `https://i.ibb.co/qC1mDJ8/ffff.jpg`;
+const destiny = [
+  `тебе сегодня повезет`,
+  `ожидает неудача`,
+  `ты встретишься с Федюком`,
+  `у тебя вырастут волосы`,
+  `твои проблемы решатся сами собой`,
+  `ты провалишь задание`,
+  `ты помоешься`,
+  `тебе понравится аниме`,
+  `ты наступишь на собачье говно`,
+  `ты послужишь Родине`,
+];
+const pop = document.querySelector(`.pop`);
+const popBtn = document.querySelector('.pop_button');
+const popImg = document.querySelector(`.pop_img`);
+const popText = document.querySelector(`.pop_inside_hello`);
+const popHeaderText = document.querySelector(`.pop_header_text`);
+const popInsideLink = document.querySelector(`.pop_inside_link`);
+const popInsideText = document.querySelector(`.pop_inside_text`);
+const popInsideTextInvisible = document.querySelector(
+  `.pop_inside_text_invisible`
+);
+const colorSwitcher = document.querySelector(`.header_text_color_switcher`);
+const colorCleaner = document.querySelector(`.header_text_color_cleaner`);
+const destinyChanger = document.querySelector(`.header_text_destiny`);
+const speakWithGod = document.querySelector(`.header_text_speak`);
+const body = document.querySelector(`.body`);
+const header = document.querySelector(`.header`);
+const footer = document.querySelector(`.footer`);
+const footerEnding = document.querySelector(`.footer_ending`);
+const year = document.querySelector(`.footer_ending_year`);
+const author = document.querySelector(`.footer_ending_author`);
+const shadow = document.querySelector(`.shadow`);
+const secretCard = document.querySelector(`.secret_card`);
+const secretBtn = secretCard.querySelector(`.secret_button`);
+const cardText = document.querySelector(`.card_text`);
+const cardPhoto = document.querySelector(`.card_photo`);
+const secretCardDestiny = document.querySelector(`.secret_card_destiny`);
+const secretCardDestinyText = document.querySelector(
+  `.secret_card_destiny_text`
+);
+const speakWithGodForm = document.querySelector(`.formContainer`);
